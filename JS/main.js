a308bd4745710b4855608c56126922ff498479b9
FilippoBid <FilippoPerrone1997@gmail.com>
2022-11-24 10:38:21 +0100

    fine esercizio

--- JS/main.js
+++ JS/main.js
@@ -1,30 +1,60 @@
 const { createApp } = Vue;
 
 createApp({
-  
-  data () {
-  
-    return {
-        instrumentList: [
-            {instrument:"Voce",
-             recorded:false},
-            {instrument:"Guitar",
-             recorded:false},
-            {instrument:"Bass",
-             recorded:false},
-            {instrument:"synth",
-             recorded:false},
-            {instrument:"Drum",
-             recorded:false},
-            {instrument:"Second Voice",
-             recorded:false},
-            {instrument:"Solo Guitar",
-             recorded:false},
-           
-        ]
-    };
-  },
-  methods:{
 
-  }
+    data() {
+
+        return {
+            instrumentList: [
+                {
+                    instrument: "Voce",
+                    recorded: false
+                },
+                {
+                    instrument: "Guitar",
+                    recorded: false
+                },
+                {
+                    instrument: "Bass",
+                    recorded: false
+                },
+                {
+                    instrument: "synth",
+                    recorded: false
+                },
+                {
+                    instrument: "Drum",
+                    recorded: false
+                },
+                {
+                    instrument: "Second Voice",
+                    recorded: false
+                },
+                {
+                    instrument: "Solo Guitar",
+                    recorded: false
+                },
+
+            ]
+        };
+    },
+    methods: {
+        clickDelete(elementDelete){
+            const confirmed = confirm("vuoi cancellare ?");
+            if(confirmed){
+                this.instrumentList.splice(elementDelete,1);
+            }
+
+                
+
+
+        },
+        songName(){
+            const nomeCanzone = prompt("inserire titolo canzone");
+            return(nomeCanzone);
+             
+
+        }
+
+    }
 }).mount("#app")
